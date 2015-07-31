d2d059c1fc04e0835f8f6f050a45fcdfbdb72a5a
songjiayang <songjiayang@weidaxue.me>
2015-07-31 14:50:48 +0800

    Add react router
    1. add a default router structer.
    2. add some default pages, eg: login,404,dashboard.

--- app/main.js
+++ app/main.js
@@ -1,4 +1,28 @@
 /** @jsx React.DOM */
-var React = require('react');
-var App = require('./App.js');
-React.render(<App/>, document.body);
+var React, App, Login, Router, NotFound;
+
+React = require('react');
+Router = require('react-router');
+
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var DefaultRoute = Router.DefaultRoute;
+
+AppRoot = require('./root.js');
+Login = require('./components/login.js');
+Dashboard = require('./components/dashboard.js');
+NotFound = require('./components/404.js');
+
+var routes = (
+  <Route name='root' path='/'>
+    <Route handler={Login} path='/s/login'/>
+    <Route handler={Dashboard} path='/dashboard'/>
+    <DefaultRoute handler={AppRoot}/>
+    <NotFoundRoute handler={NotFound} />
+  </Route>
+);
+
+
+Router.run(routes, Router.HashLocation, function (Handler) {
+  React.render(<Handler/>, document.body);
+});
